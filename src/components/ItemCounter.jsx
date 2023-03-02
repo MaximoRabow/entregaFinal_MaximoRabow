cdbaf2c0a2dbd6d31cf618faa85f82e91fa5326e
MaximoRabow <113936807+MaximoRabow@users.noreply.github.com>
2023-03-02 14:14:12 -0300

    varios

--- src/components/ItemCounter.jsx
+++ src/components/ItemCounter.jsx
@@ -1,10 +1,28 @@
+import { Box, useState } from '@chakra-ui/react';
 import React from 'react'
 
+
 const ItemCounter = () => {
+  const [valor, setValor] = useState (0);
+
+  const sum = () => {
+    setValor (valor + 1);
+  }
+
+  const res = () => {
+    setValor (valor - 1);
+  }
+
+  const reset = () => {
+    setValor (0);
+  }
+
   return (
-    <div>
-
-
+    <div className='counter'>
+      <Box>{valor}</Box>
+      <Button onClick={sum} >+</Button>
+      <Button onClick={res} >-</Button>
+      <Button onClick={reset} >Reset</Button>
         
     </div>
   )
